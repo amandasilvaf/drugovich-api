8b1de8d4d9c34336b2975038aef0b7339c77f433
Amanda S. Ferreira <amandasilvaf1995@gmail.com>
2023-01-09 09:12:17 -0300

    Creates CustomAbility  middleware to customize error message

---
+++ app/Http/Middleware/CustomAbility.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CustomAbility
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next, $permission)
+    {
+        if (!$request->user()->can($permission)) {
+            return response()->json([
+                'error' => 'Acesso negado. Você não tem permissão para acessar esse recurso.'
+            ], 403);
+        }
+    
+        return $next($request);
+    }
+}
